51d15e2457515599e86c79fb51cccf29bdf50cee
bsoft-daniel <daniel@dbcyelagiri.edu.in>
2026-09-17 14:59:23 +0530

    connection string changed

--- TeamContributionManagementSystem.Infrastructure/InfrastructureServiceCollectionExtensions.cs
+++ TeamContributionManagementSystem.Infrastructure/InfrastructureServiceCollectionExtensions.cs
@@ -19,7 +19,7 @@ public static class InfrastructureServiceCollectionExtensions
     public static IServiceCollection AddApplicationAndInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+            options.UseNpgsql(configuration.GetConnectionString("ConnString")));
 
         services.AddScoped<IUnitOfWork>(provider => provider.GetRequiredService<ApplicationDbContext>());
         services.AddScoped<IMemberRepository, MemberRepository>();
