dd5f4f0e8ffd588ce80782370238e737558444b6
Lourdhu Sylvester <lourdhusylvester@dbcyelagiri.edu.in>
2026-09-25 12:28:30 +0530

    changes in the user rights

--- TeamContributionManagementSystem.Application/Interfaces/Services/IRoleRightsService.cs
+++ TeamContributionManagementSystem.Application/Interfaces/Services/IRoleRightsService.cs
@@ -5,11 +5,7 @@ namespace TeamContributionManagementSystem.Application.Interfaces.Services;
 
 public interface IRoleRightsService
 {
-    Task<IReadOnlyCollection<RoleRightDto>> GetAllAsync(CancellationToken cancellationToken = default);
-    Task<IReadOnlyCollection<RoleRightDto>> GetByRoleAsync(string roleName, CancellationToken cancellationToken = default);
+    Task<IReadOnlyCollection<RoleRightDto>> GetAllRoleRightAsync(CancellationToken cancellationToken = default);
+    Task<IReadOnlyCollection<RoleRightDto>> GetRoleRightAsyncByRole(string roleName, CancellationToken cancellationToken = default);
     Task SaveRoleRightsAsync(UpdateRoleRightsRequestDto request, string? user = null, CancellationToken cancellationToken = default);
-
-    // Standardized terminology
-    Task<IReadOnlyCollection<RoleRightDto>> GetAllRoleRightAsync(CancellationToken cancellationToken = default) => GetAllAsync(cancellationToken);
-    Task<IReadOnlyCollection<RoleRightDto>> GetRoleRightAsyncByRole(string roleName, CancellationToken cancellationToken = default) => GetByRoleAsync(roleName, cancellationToken);
 }
